cfabc52ba88fbf3b92f61ef39812c9141d647cd7
Shubha Shree R <shubhashree1805@gmail.com>
2025-01-10 14:15:52 +0530

    while loop concept

--- Easy.js
+++ Easy.js
@@ -38,3 +38,93 @@ function EvenNumberDes(){
     }
 }
 EvenNumberDes();
+
+
+// WAP to print the odd number from 1 to 10 ;
+
+function OddNumberAss(){
+    for(let i=1; i<=10; i++){
+        if(i%2 != 0){
+            console.log(i);
+        }
+    }
+}
+OddNumberAss();
+
+// WAP to print the odd number from 10 to 1 ;
+
+function OddNumberDes(){
+    for(let i=10; i>=1; i--){
+        if(i%2 != 0){
+            console.log(i);
+        }
+    }
+}
+OddNumberDes();
+
+// WAP to print the  number from 1 to 10 using while ;
+
+let n=1;
+
+while(n <= 10){
+    console.log(n);
+    n++
+}
+
+
+// WAP to print the  number from 10 to 1 using while ;
+
+let num=10;
+
+while(num >= 1){
+    console.log(num);
+    num--
+}
+
+// WAP to print the even number from 1 to 10 using while ;
+
+let evennum=1;
+
+while(evennum <= 10){
+    if(evennum%2 == 0){
+        console.log(evennum);
+    
+    }
+    evennum++
+}
+
+// WAP to print the even  number from 10 to 1 using while ;
+
+let oddnum=10;
+
+while(oddnum >= 1){
+    if(oddnum%2 == 0){
+        console.log(oddnum);
+       
+    }
+    oddnum--
+}
+
+// WAP to print the even number from 1 to 10 using while ;
+
+let evennumdes=1;
+
+while(evennumdes <= 10){
+    if(evennumdes%2 != 0){
+        console.log(evennumdes);
+    
+    }
+    evennumdes++
+}
+
+// WAP to print the even  number from 10 to 1 using while ;
+
+let oddnumdes=10;
+
+while(oddnumdes >= 1){
+    if(oddnumdes%2 != 0){
+        console.log(oddnumdes);
+       
+    }
+    oddnumdes--
+}
